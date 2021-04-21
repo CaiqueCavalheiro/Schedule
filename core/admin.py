56650214b1286a77a7c40dc8928ecf91aa912104
Caique <caiqueword@gmail.com>
2021-04-21 01:40:19 -0300

    Make a table of events to work with schedule

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao') #adiciona ao menu principal do evento os dados de data de criação e ultima alteração
+    list_filter = ['titulo', 'data_evento']
+
+admin.site.register(Evento, EventoAdmin)
