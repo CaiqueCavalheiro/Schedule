eae041b9d039c7bf3af227b5638a7213abca395c
Caique <caiqueword@gmail.com>
2021-04-23 15:18:11 -0300

    Creaty list of events

--- core/models.py
+++ core/models.py
@@ -15,3 +15,7 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.titulo #retorna o nome do evento como o titulo dado
+
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y  %H:%M Hrs')
